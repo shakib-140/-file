f7c6ba96bc313e6bebe412cb836ce026584ae1d0
Shakirrojjaman <95244399+shakib-140@users.noreply.github.com>
2021-12-25 12:25:14 +0600

    Add files via upload

---
+++ read in a file.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    FILE *file4;
+    char ch;
+    file4=fopen("name and age.txt","r");
+    if(file4==NULL)
+    {
+        printf("file doesn't exist");
+    }
+    else
+    {
+        printf("file is open\n");
+        while(!feof(file4))
+        {
+            ch=fgetc(file4);
+            printf("%c",ch);
+        }
+
+        printf("file is opened successfully");
+    }
+
+    getch();
+}
